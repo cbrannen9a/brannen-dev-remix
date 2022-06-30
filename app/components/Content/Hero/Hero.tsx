cc081636b5064ca25dcbfa8bc7332001c7e58165
Chris Brannen <cbrannen@outlook.com>
2022-06-30 11:24:49 +0100

    Hero image

--- app/components/Content/Hero/Hero.tsx
+++ app/components/Content/Hero/Hero.tsx
@@ -1,47 +1,54 @@
 import type { HeroContent } from "~/types";
 import { type FC } from "react";
-// import Image from "next/image";
+
 import { PortableText } from "@portabletext/react";
-import { Ctas } from "../Common";
+import { Ctas, Image } from "../Common";
 
 const Hero: FC<
   Pick<HeroContent, "heading" | "subHeading" | "tagline" | "ctas" | "image">
 > = ({ heading, subHeading, tagline, ctas, image }) => (
   <section>
-    <div className="max-w-7xl mx-auto relative bg-white overflow-hidden p-1 sm:p-4">
-      <>
-        <div className="relative z-10 px-8 pt-8 bg-white lg:max-w-2xl lg:w-full ">
-          <div className="text-left">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">{heading}</span>
-              <span className="block text-blue-600 xl:inline">
-                {subHeading}
-              </span>
-            </h1>
-            <div className="mb-2 mt-3 text-base text-gray-500 md:text-xl lg:mx-0">
-              {tagline ? <PortableText value={tagline} /> : null}
-            </div>
-
-            {ctas && (
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <Ctas ctas={ctas} />
+    <div className="relative pt-[120px] lg:pt-[150px] pb-[110px] bg-white">
+      <div className="container">
+        <div className="flex flex-wrap -mx-4">
+          <div className="w-full lg:w-5/12 px-4">
+            <div className="hero-content">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">{heading}</span>
+                <span className="block text-blue-600 xl:inline">
+                  {subHeading}
+                </span>
+              </h1>
+              <div className="mb-2 mt-3 text-base text-gray-500 md:text-xl lg:mx-0">
+                {tagline ? <PortableText value={tagline} /> : null}
               </div>
-            )}
+
+              {ctas && (
+                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                  <Ctas ctas={ctas} />
+                </div>
+              )}
+
+              {image && (
+                <div className="w-full lg:w-6/12 px-4">
+                  <div className="lg:text-right lg:ml-auto">
+                    <div className="relative inline-block z-10 pt-11 lg:pt-0">
+                      <Image
+                        className="max-w-full lg:ml-auto"
+                        value={image}
+                        alt={image.alt}
+                        loading="eager"
+                        width={350}
+                        height={250}
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </>
-      {image && (
-        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 p-4 overflow-hidden">
-          {/* <Image
-            className="h-48 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src={image.asset.url}
-            alt={image.alt}
-            layout="responsive"
-            width={350}
-            height={250}
-          /> */}
-        </div>
-      )}
+      </div>
     </div>
   </section>
 );
