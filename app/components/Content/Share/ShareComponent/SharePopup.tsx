147aad470fff21fccf2c146f01109d598c18e340
Chris Brannen <cbrannen@outlook.com>
2022-07-25 19:37:53 +0100

    Share

--- app/components/Content/Share/ShareComponent/SharePopup.tsx
+++ app/components/Content/Share/ShareComponent/SharePopup.tsx
@@ -42,16 +42,13 @@ const SharePopup: FC<WithColors<Props>> = ({
   };
 
   return (
-    <div
-      className="fixed z-10 inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-      onClick={onClose}
-    >
+    <div className="fixed z-10 inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
       <div className="fixed z-20 inset-0 overflow-y-auto">
         <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
           <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-              <div className="mt-3  inline-flex text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className="m-2 inline-flex text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg leading-6 mb-2 font-medium text-gray-900 prose">
                   {shareData.title}
                 </h3>
                 <button className="ml-4" onClick={onClose}>
@@ -73,29 +70,36 @@ const SharePopup: FC<WithColors<Props>> = ({
                   </div>
                 </button>
               </div>
-              <div className="mt-2">
+              <div className="m-2">
                 {state === "error" ? (
-                  // <Alert
-                  //   glyph="infoRoundNoFill"
-                  //   glyphColor={Color.ROOF_TERRACOTTA}
-                  //   backgroundStyles={[errorStyles.background]}
-                  //   borderStyles={[errorStyles.border]}
-                  //   containerStyles={[errorStyles.container]}
-                  //   textStyles={[errorStyles.text]}
-                  //   textBlocks={[
-                  //     {
-                  //       text: "Unable to copy BuildABet to clipboard, ",
-                  //       styles: [textStyles.bold],
-                  //     },
-                  //     {
-                  //       text: "please manually copy the url to share.",
-                  //     },
-                  //   ]}
-                  // />
-                  <div />
+                  <div className="mb-2 p-2 border-2 rounded-md border-red-600">
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <svg
+                        className="h-6 w-6 text-red-600"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                        />
+                      </svg>
+                    </div>
+                    <div className="m-2">
+                      <p className="text-sm text-red-500 text-center prose">
+                        Unable to copy to clipboard, please manually copy the
+                        url to share.
+                      </p>
+                    </div>
+                  </div>
                 ) : null}
                 <input
-                  className="w-full mr-8 overflow-hidden"
+                  className="w-full mb-4 p-2 overflow-hidden border-2 rounded-md border-gray-500 prose"
                   value={shareData.url}
                   readOnly
                 />
@@ -104,18 +108,30 @@ const SharePopup: FC<WithColors<Props>> = ({
                     backgroundColor: colors.primary.hex,
                     color: colors.primaryText.hex,
                   }}
-                  className="w-full mx-auto inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="w-full mx-auto inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={copyClicked}
                 >
                   {getButtonText(state)}
-                  {/* {state === "success" ? (
-              <Glyph
-                src={Glyph.icons.tick}
-                className={css(styles.tickIcon)}
-                size={12}
-                color={Color.WHITE}
-              />
-            ) : null} */}
+                  {state === "success" ? (
+                    <svg
+                      className="ml-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20px"
+                      height="20px"
+                      viewBox="-0.5 -0.5 85 65"
+                    >
+                      <g>
+                        <path
+                          d="M 2 32 L 24.93 54.93 Q 32 62 38.4 54.32 L 82 2"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="8"
+                          strokeMiterlimit="10"
+                          pointerEvents="stroke"
+                        />
+                      </g>
+                    </svg>
+                  ) : null}
                 </button>
               </div>
             </div>
@@ -133,131 +149,5 @@ interface Props {
   onClose: () => void;
   onError?: (error?: unknown) => void;
 }
-// const {
-//   BREAKPOINTS: { MEDIUM },
-// } = Size;
-
-// const styles = StyleSheet.create({
-//   blackOverlay: {
-//     position: "fixed",
-//     width: "100%",
-//     opacity: "0.5",
-//     margin: "0",
-//     height: "200vh",
-//     zIndex: 92,
-//     backgroundColor: Color.TUNDORA,
-//   },
-//   container: {
-//     backgroundColor: Color.WHITE,
-//     borderRadius: "16px 16px 0 0",
-//     overflow: "hidden",
-//     position: "fixed",
-//     width: "100%",
-//     pointerEvents: "none",
-//     bottom: "0",
-//     zIndex: 100,
-//     [`@media (min-width: ${MEDIUM.minWidth}px)`]: {
-//       top: "50%",
-//       left: "50%",
-//       transform: "translate(-50%, -50%)",
-//       width: "375px",
-//       bottom: "auto",
-//       borderRadius: "16px",
-//     },
-//   },
-//   header: {
-//     display: "flex",
-//     backgroundColor: Color.WHITE,
-//     boxShadow: "0px 0px 12px rgba(0, 0, 0, 0.08)",
-//     overflow: "hidden",
-//     marginBottom: "16px",
-//   },
-//   headerText: {
-//     fontFamily: Font.SKY_MEDIUM,
-//     lineHeight: "24px",
-//     fontSize: "16px",
-//     color: Color.COD_GRAY,
-//     width: "100%",
-//     textAlign: "center",
-//     whiteSpace: "nowrap",
-//     textOverflow: "ellipsis",
-//     padding: "20px 0px 20px 42px",
-//     overflow: "hidden",
-//   },
-//   closeButton: {
-//     display: "inline-flex",
-//     alignItems: "center",
-//     padding: "20px 16px",
-//     ":hover": {
-//       background: "none",
-//     },
-//   },
-//   content: {
-//     display: "flex",
-//     backgroundColor: Color.WHITE,
-//     flexDirection: "column",
-//     paddingLeft: "16px",
-//     paddingRight: "16px",
-//   },
-//   genericButtonOverrides: {
-//     border: "none",
-//     alignItems: "center",
-//     margin: "0",
-//     padding: "0",
-//     pointerEvents: "auto",
-//     ":hover": {
-//       background: "none",
-//     },
-//   },
-//   copyLinkButton: {
-//     color: Color.WHITE,
-//     background: Color.BARTHEZ,
-//     height: "48px",
-//     width: "100%",
-//     marginBottom: "16px",
-//     borderRadius: "4px",
-//     fontSize: "16px",
-//     lineHeight: "24px",
-//     fontFamily: Font.SKY_MEDIUM,
-//     ":hover": {
-//       background: Color.BARTHEZ,
-//     },
-//   },
-//   url: {
-//     color: Color.DAVYS_GREY,
-//     cursor: "text",
-//     border: `1px solid ${Color.GALLERY}`,
-//     padding: "12px 16px 12px 16px",
-//     marginBottom: "8px",
-//     pointerEvents: "auto",
-//     whiteSpace: "nowrap",
-//     overflow: "hidden",
-//   },
-//   icon: {
-//     display: "flex",
-//   },
-//   tickIcon: {
-//     marginLeft: "5px",
-//   },
-// });
-
-// const errorStyles = StyleSheet.create({
-//   container: {
-//     margin: "0 0 12px 0",
-//   },
-//   background: {
-//     backgroundColor: Color.CAROUSEL_PINK,
-//   },
-//   border: { border: `1px solid ${Color.ROOF_TERRACOTTA}` },
-//   text: {
-//     color: Color.ROOF_TERRACOTTA,
-//   },
-// });
-
-// const textStyles = StyleSheet.create({
-//   bold: {
-//     fontFamily: Font.SKY_MEDIUM,
-//   },
-// });
 
 export default SharePopup;
