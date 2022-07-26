1f3c43bcffa0eaf46b26c3f8ad27f779ba57494a
Chris Brannen <cbrannen@outlook.com>
2022-07-26 13:24:01 +0100

    Fix query errors

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -4,16 +4,16 @@ import { Link, useLoaderData, useOutletContext } from "@remix-run/react";
 import { Content } from "~/components";
 
 import { getPageData, queryHelper } from "~/lib";
-import type { ContextData, Colors } from "~/types";
+import type { ContextData, Colors, PageContent, PreviewContent } from "~/types";
 
 export const meta: MetaFunction = ({ data, parentsData }) => {
-  const { pageData } = data;
-  if (!pageData) {
+  if (!data || !data.pageData) {
     return {
       title: `${parentsData?.root.title} | No title`,
       description: "No description found",
     };
   }
+  const { pageData } = data;
   return {
     title: `${parentsData?.root.title} | ${pageData.title}`,
     description: `${
@@ -32,7 +32,9 @@ export const loader: LoaderFunction = async ({ request, params }) => {
   const { query, queryParams } = queryHelper(params["*"]);
 
   const { pageData, previewData } = await getPageData(query, queryParams);
-
+  if (!pageData) {
+    throw new Error(`PageData missing for ${requestUrl}`);
+  }
   return {
     pageData,
     previewData,
@@ -42,15 +44,24 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 
 export const ErrorBoundary = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col m-2 h-screen text-center items-center">
       <div>Something's gone wrong</div>
-      <Link to="/">Home</Link>
+      <Link
+        to="/"
+        className="mt-2 p-3 flex items-center rounded-md border-2 border-gray-300 hover:bg-gray-50"
+      >
+        <span className="mx-4 text-base font-medium">Home</span>
+      </Link>
     </div>
   );
 };
 
 export default function Body() {
-  const { pageData, previewData, requestUrl } = useLoaderData();
+  const { pageData, previewData, requestUrl } = useLoaderData<{
+    pageData: PageContent | null;
+    previewData: Record<string, PreviewContent[]>;
+    requestUrl: string;
+  }>();
   const { colors, sanityDataset, sanityProjectId } = useOutletContext<{
     colors: Colors;
     sanityDataset: string;
@@ -59,7 +70,7 @@ export default function Body() {
 
   const contextData: ContextData = {
     url: requestUrl,
-    title: pageData.title,
+    title: pageData?.title ?? "",
   };
 
   return pageData ? (
